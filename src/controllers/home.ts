55213a58fbcb7e1bfd22b585ae4e76a418b91902
s-kog <36532564+s-kog@users.noreply.github.com>
2018-10-20 07:17:14 +0530

    created offer model & integrated in home

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,11 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import Offer from "../models/Offer";
 
 /**
  * GET /
  * Home page.
  */
-export let index = (req: Request, res: Response) => {
-  res.render("home", {
-    title: "Home"
+export let index = (req: Request, res: Response, next: NextFunction) => {
+  Offer.find()
+  .limit(10)
+  .exec((err, offerList) => {
+    if (err) { return next(err); }
+    res.render("home", {
+      title: "Home",
+      offerList: offerList
+    });
   });
 };
